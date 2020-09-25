1c0cb71d2264bebd78a70e6aa2e55a54a73a110d
erickleaofa7 <47090978+erickleaofa7@users.noreply.github.com>
2020-09-25 12:22:05 -0500

    segundo cambio

---
+++ XamV/XamV/Infraestructure/InstanceLocator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using XamV.ViewModels;
+
+namespace XamV.Infraestructure
+{
+   
+    public class InstanceLocator
+    {
+
+        #region propiety
+        public MainViewModel Main 
+        { 
+            get; set; 
+        }
+
+        #endregion
+
+        #region constructor
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+        #endregion
+    }
+}
---
+++ XamV/XamV/ViewModels/MainViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamV.ViewModels
+{
+    public class MainViewModel
+    {
+    }
+}
