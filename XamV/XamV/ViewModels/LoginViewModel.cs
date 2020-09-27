a979977fbcb2b4cfb61e72bf14e40794b8059f0b
erickleaofa7 <47090978+erickleaofa7@users.noreply.github.com>
2020-09-26 22:55:34 -0500

    cambio tres

---
+++ XamV/XamV/ViewModels/LoginViewModel.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.Command;
+using Xamarin.Forms;
+using System.ComponentModel;
+
+namespace XamV.ViewModels
+{
+    
+
+    public class LoginViewModel : BaseViewModel
+    {
+        #region Atributos
+        private string Email { get; set; }
+        private string Password { get; set; }
+        private bool IsRunning { get; set; }
+        private bool IsEnabled { get; set; }
+        private bool IsRemember { get; set; }
+        #endregion
+
+        #region Prpiedades
+        public string email
+        {
+            get { return this.Email; }
+            set { Email = value;OnPropertyChanged(); }
+        }
+        public string password 
+        {
+            get { return this.Password; }
+            set { Password = value;OnPropertyChanged(); }
+        }
+        public bool isRunnig 
+        {
+            get { return this.IsRunning; }
+            set { IsRunning = value; OnPropertyChanged(); }
+            
+        }
+        public bool isRemember { get; set; }
+        public bool isEnabled 
+        {
+            get { return this.IsEnabled; }
+            set { IsEnabled = value;OnPropertyChanged(); }
+            
+        }
+
+        #endregion
+
+        #region Comandos
+        public ICommand loginComand 
+        { get
+            {
+                return new RelayCommand(Login);
+            }
+
+            }
+
+      
+
+        private async void Login()
+        {
+            if(String.IsNullOrEmpty(this.email)){
+                await Application.Current.MainPage.DisplayAlert("Error:", "ingrese un e-mail por favor", "ok");
+                return;
+            }
+
+            if (String.IsNullOrEmpty(this.password))
+            {
+                await Application.Current.MainPage.DisplayAlert("Error:", "ingrese su contraseña", "ok");
+                return;
+            }
+
+            if(this.email != "erick.alvear7@gmail.com" || this.password != "erick")
+            {
+                await Application.Current.MainPage.DisplayAlert("Error:", "e-mail o password incorrecto", "aceptar");
+                this.password = string.Empty;
+                this.email = string.Empty;
+                return;
+            }
+
+        }
+
+        #endregion
+
+        #region Constructor
+        public LoginViewModel()
+        {
+            this.isRemember = true;
+            this.isEnabled = true;
+        }
+
+        #endregion
+    }
+}
--- XamV/XamV/ViewModels/MainViewModel.cs
+++ XamV/XamV/ViewModels/MainViewModel.cs
@@ -1,10 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using XamV.ViewModels;
 
 namespace XamV.ViewModels
 {
     public class MainViewModel
     {
+        #region viewModels
+        public LoginViewModel Login 
+        { 
+            get; 
+            set; 
+        
+        }
+        #endregion
+
+        #region Constrctores
+        public MainViewModel()
+        {
+            this.Login = new LoginViewModel();
+        }
+        #endregion
+
     }
 }
